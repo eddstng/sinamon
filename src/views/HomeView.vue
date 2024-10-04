59f3d853ebc07083667176e185b68fe7f5419027
Edmond Lee <eddstng@gmail.com>
2024-10-03 17:56:37 -0700

    add tailwindcss

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="text-3xl font-bold underline mb-10">TailwindCSS!</h1>
   </main>
 </template>
